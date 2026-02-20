827a0cb61a7d7138854943351a95184cc1da39fd
thekrishnamsingh <thekrishnamsingh@gmail.com>
2026-02-20 23:47:36 +0530

    student

--- myapp/admin.py
+++ myapp/admin.py
@@ -5,6 +5,7 @@ from .models import Student,Faculty
 # Register your models here.
 #admin.site.register(Student)
 #admin.site.register(Faculty)
+#admin.site.register(Student)
 
 class AdminStudent(admin.ModelAdmin):
     list_display=['name','rollno','course','teacher','semester','branch','present','absent','sr']

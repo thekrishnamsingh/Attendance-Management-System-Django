ff766eec2b55b50a5ed4da711c045fa7d9ada497
thekrishnamsingh <thekrishnamsingh@gmail.com>
2026-02-21 21:54:47 +0530

    commentout

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,12 +2,6 @@ from django.contrib import admin
 from .models import Student,Faculty
 
 
-# Register your models here.
-#admin.site.register(Student)
-#admin.site.register(Faculty)
-
-
-
 class AdminStudent(admin.ModelAdmin):
     list_display=['name','rollno','course','teacher','semester','branch','present','absent','sr']
 class AdminFaculty(admin.ModelAdmin):

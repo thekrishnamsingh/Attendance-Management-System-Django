6b53531ac9c4db4244e5fa7666c2d4a0a29dfa6c
thekrishnamsingh <thekrishnamsingh@gmail.com>
2026-02-23 23:17:18 +0530

    commentout

--- myapp/admin.py
+++ myapp/admin.py
@@ -6,6 +6,8 @@ from .models import Student,Faculty
 
 class AdminStudent(admin.ModelAdmin):
     list_display=['name','rollno','course','teacher','semester','branch','present','absent','sr']
-
+class AdminFaculty(admin.ModelAdmin):
+    list_display=['name','username','password','dob','qualification','mobile','gender']
+    
 admin.site.register(Student,AdminStudent)
 admin.site.register(Faculty,AdminFaculty)

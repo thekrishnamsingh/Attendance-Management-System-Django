33d3bfaa0a5d3bb673c6c0549086459cbd438062
thekrishnamsingh <thekrishnamsingh@gmail.com>
2026-02-24 23:15:44 +0530

    admin site

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,11 +1,1 @@
-from django.contrib import admin
-from .models import Student,Faculty
 
-
-
-
-class AdminStudent(admin.ModelAdmin):
-    list_display=['name','rollno','course','teacher','semester','branch','present','absent','sr']
-class AdminFaculty(admin.ModelAdmin):
-    list_display=['name','username','password','dob','qualification','mobile','gender']
-    
